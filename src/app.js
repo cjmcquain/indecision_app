e0eee9ce489098c2b7d678c3b678e683e681dd01
Charles McQuain <charlesmcquain@gmail.com>
2018-11-11 22:29:50 -0800

    Added header to home page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 console.log("App.js is running");
 
-var template = <p>This is JSX from app.js!</p>;
+var template = <h1>Indecision App</h1>;
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
