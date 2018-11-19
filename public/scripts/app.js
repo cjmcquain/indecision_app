ac1c85dcac2f59629621c2e55fbd2922af057851
Charles McQuain <charlesmcquain@gmail.com>
2018-11-18 21:11:08 -0800

    Created counter-example file and moved counter app data to that file

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,72 +1,46 @@
-"use strict";
+'use strict';
 
-console.log("App.js is running");
+console.log('App is running!');
 
-/* const app = {
+var app = {
   title: 'Indecision App',
-  subtitle: 'Best app ever',
-  options: ['Item one', 'Item two']
-};
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    {(app.subtitle) && <p>{app.subtitle}</p>}
-    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
-*/
-
-var count = 0;
-var addOne = function addOne() {
-  count++;
-  renderCounterApp();
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
 
-var minusOne = function minusOne() {
-  count--;
-  renderCounterApp();
-};
-
-var reset = function reset() {
-  count = 0;
-  renderCounterApp();
-};
-
-var appRoot = document.getElementById('app');
-
-var renderCounterApp = function renderCounterApp() {
-  var templateTwo = React.createElement(
-    "div",
+var template = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options' : 'No options'
+  ),
+  React.createElement(
+    'ol',
     null,
     React.createElement(
-      "h1",
+      'li',
       null,
-      "Count: ",
-      count,
-      " "
+      'Item one'
     ),
     React.createElement(
-      "button",
-      { onClick: addOne },
-      "+1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: minusOne },
-      "-1"
-    ),
-    React.createElement(
-      "button",
-      { onClick: reset },
-      "reset"
+      'li',
+      null,
+      'Item two'
     )
-  );
+  )
+);
 
-  ReactDOM.render(templateTwo, appRoot);
-};
-
-renderCounterApp();
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
